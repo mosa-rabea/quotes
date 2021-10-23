5fae1435e7f8de7f871a6f7516e132117d48702d
mosa-rabea <mosa@protonmail.com>
2021-10-23 17:18:30 +0300

    lab09

--- app/src/main/java/quotes/App.java
+++ app/src/main/java/quotes/App.java
@@ -6,10 +6,9 @@ package quotes;
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class App {
@@ -18,13 +17,33 @@ public class App {
     public static void main(String[] args) {
 
         try {
-            FileReader jsonFile = new FileReader("recentquotes.json");
+            URL url = new URL("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en");
+            HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+            connect.setRequestMethod("GET");
+            connect.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            int status = connect.getResponseCode();
 
-           ArrayList<Quotes> quotes= readJson(jsonFile);
-            int r = (int) (Math.random() * (quotes.size()));
-            System.out.println(quotes.get(r).toString());
+            if(status == 200){
+                InputStream input = connect.getInputStream();
+                InputStreamReader inputRead = new InputStreamReader(input);
+                BufferedReader bufRead = new BufferedReader(inputRead);
+                String line = bufRead.readLine();
 
-        } catch (FileNotFoundException e) {
+                System.out.println(line);
+
+                bufRead.close();
+            } else{
+                System.out.println("LOCALE");
+                FileReader jsonFile = new FileReader("recentquotes.json");
+
+                ArrayList<Quotes> quotes= readJson(jsonFile);
+                int r = (int) (Math.random() * (quotes.size()));
+                System.out.println(quotes.get(r).toString());
+            }
+
+            connect.disconnect();
+
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
---
+++ lab09.md
@@ -0,0 +1,34 @@
+# quotes
+
+
+# Quotes
+
+#### description 
+Continue building on your work from lab 8. If your partner was the one who created the repo in that lab, create a fork of their repo and clone that fork to hold your work for today.
+
+Start by refactoring anything that you don’t like about your solution from yesterday.
+When running the application, don’t read in the quotes file. Instead, make a request to an API to get a random quote.
+please do not delete your existing code, look at the very next feature task, you will still need that code
+
+
+
+#### My work :
+* My partner : Malik Swayyed .
+
+* I was the navigater and malik was driver .
+
+
+
+
+#### installing Gson dependancies
+
+1. open the application with a text editor.
+2. open the build.gradle file.
+3. under dependancies add this line:
+
+```
+    implementation 'com.google.code.gson:gson:2.8.7'
+```
+
+4. rebuild the gradle and your Gson is ready to use.
+
